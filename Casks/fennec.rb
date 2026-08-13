bb43376ed20f26a894da0da5c74279bbb16d897c
Karaage <yknoaki@gmail.com>
2026-08-13 23:02:22 +0900

    fennec: fix release asset URL and reset version to 0.0.1

--- Casks/fennec.rb
+++ Casks/fennec.rb
@@ -1,8 +1,8 @@
 cask "fennec" do
-  version "2.0.0"
+  version "0.0.1"
   sha256 "cbe3afc1eeba1b00d4b9b8926147794de98de8d42156ac429c434a6f72e847a2"
 
-  url "https://github.com/krgpi/fennec/releases/download/v#{version}/Fennec.zip"
+  url "https://github.com/krgpi/fennec/releases/download/v#{version}/Fennec_#{version}_arm64.zip"
   name "Fennec"
   desc "Local transcription & AI meeting notes for macOS"
   homepage "https://github.com/krgpi/fennec"
